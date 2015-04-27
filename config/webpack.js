86253e19556859737ff04efbb0e3d80afc5a8dcd
Michiel Sikma <michiel@privax.com>
2015-04-27 14:29:28 +0100

    Slight clarification

--- config/webpack.js
+++ config/webpack.js
@@ -11,20 +11,20 @@ module.exports = {
   },
   module: {
     loaders: [
-      // Comment out the following section and uncomment the one after that
-      // to test CSS-in-JS rather than CSS in a separate file.
+      /*{
+        // If this section is active, CSS is in the JS.
+        // The test image is included only once, without duplication.
+        test: /\.scss$/,
+        loader: 'style!css!sass?outputStyle=expanded'
+      },*/
       {
+        // If this section is active, CSS is in a separate file.
+        // The test image is included multiple times.
         test  : /\.scss$/,
         loader: ExtractTextPlugin.extract(
           'style-loader', 'css-loader!sass-loader?outputStyle=expanded'
         )
       },
-      /*
-      {
-        test: /\.scss$/,
-        loader: 'style!css!sass?outputStyle=expanded'
-      },
-      */
       {
         test: /\.png$/,
         loader: 'url-loader?mimetype=image/png'
